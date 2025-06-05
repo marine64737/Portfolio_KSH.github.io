4e7b1b02fd9c5e7fe428d847be5c4a42240374a8
marine64737 <81956909+marine64737@users.noreply.github.com>
2025-06-05 18:23:06 +0900

    Delete BoardProject/BoardProject directory
    Mistake of upload

--- BoardProject/BoardProject/src/main/java/com/example/BoardProject/Repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.BoardProject.Repository;
-
-import com.example.BoardProject.Entity.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Article, Long> {
-}
